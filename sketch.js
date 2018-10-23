555f16af3206e44f923c909f2a3ff9d85f79dafb
MatildeMammana <matilde.mammana@gmail.com>
2018-10-23 21:52:43 +0200

    Completed

--- sketch.js
+++ sketch.js
@@ -1,11 +1,36 @@
+var mySong;
+var amp;
+var gramm;
+var note;
+
 function preload(){
-  // put preload code here
+mySong = loadSound("./assets/bensound-allthat.mp3");
+gramm = loadImage ("./assets/gramm.png");
+note = loadImage ("./assets/notes.png")
+
 }
 
 function setup() {
-  // put setup code here
+
+  createCanvas (windowWidth, windowHeight);
+
+  mySong.loop();
+  amp = new p5.Amplitude();
+
 }
 
 function draw() {
-  // put drawing code here
+
+  background(224,209,183);
+
+  var vol= amp.getLevel();
+  var dim= map(vol,0,1,width/30,width/15);
+
+  image(gramm, windowWidth/2+20,70,500,500);
+  image(note,width/2,height/2-200,15+dim,15+dim);
+  image(note,width/2-100,height/2-200,15+dim,15+dim);
+  image(note,width/2-180,height/2-170,15+dim,15+dim);
+  image(note,width/2-280,height/2-120,15+dim,15+dim);
+  image(note,width/2-360,height/2-200,15+dim,15+dim);
+
 }
